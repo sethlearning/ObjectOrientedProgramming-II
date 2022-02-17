f287b8329629aa2b0662b0e1ede7a397d17ddbd1
sethvs <sethvs@users.noreply.github.com>
2022-02-17 19:30:54 +0300

    Initial commit

---
+++ Lab-0/Lab-0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!!!\n\nProgrammed by Sergey Vasin.");
+            Console.ReadKey();
+        }
+    }
+}
