cf1b253a0415d2144fa6654fb53badcdb5aeeb38
sethvs <sethvs@users.noreply.github.com>
2022-04-25 17:49:15 +0300

    Lab-6.1

---
+++ Lab-6/Lab-6.1/Form1Graphics.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab_6
+{
+    public partial class Form1 : Form
+    {
+
+        // Sin(x) - Sin(2Pi - x)
+        float f(float x) => (float)(Math.Sin(x) - Math.Sin(2 * Math.PI - x));
+
+
+        void CreateDefaultChartSeries()
+        {
+            chart.Series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());
+            chart.Series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());
+            chart.Series.Add(new System.Windows.Forms.DataVisualization.Charting.Series());
+
+            chart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart.Series[0].Color = Color.Red;
+            chart.Series[0].BorderWidth = 2;
+            chart.Series[0].LegendText = "(e^x - e^-x)/2";
+
+            chart.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart.Series[1].Color = Color.Blue;
+            chart.Series[1].BorderWidth = 1;
+            chart.Series[1].LegendText = "E x^2n+1/(2n+1)!";
+
+            chart.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart.Series[2].Color = Color.Orange;
+            chart.Series[2].LegendText = "Functions delta";
+        }
+
+        static public double formula(double x)
+        {
+            double result = 0;
+
+            result = (Math.Exp(x) - Math.Exp(-x)) / 2;
+
+            return result;
+        }
+
+        static public double series(double x)
+        {
+            double result, xp, fn;
+            result = xp = fn = default;
+            int n, N = 12;
+
+            result = x;
+            xp = x;
+            fn = 1;
+
+            for (n = 1; n <= N; n++)
+            {
+                xp *= x * x;
+                fn *= 2 * n * (2 * n + 1);
+
+                result += xp / fn;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Lab-6/Lab-6.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6
+{
+    enum FileType
+    {
+        jpeg,
+        bmp
+    }
+    enum ImagingMode
+    {
+        startup,
+        pictureBoxImage,
+        pictureBoxGraphic1,
+        chartGraphic1,
+        chartGraphic2
+    }
+    internal static class Program
+    {
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
